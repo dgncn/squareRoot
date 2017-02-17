0f4c16893895cb64f8dd5739b6a96bd027fb5835
dgncn <dgncn.ksp@gmail.com>
2017-02-17 17:34:34 +0300

    Add project files.

---
+++ karekok/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karekok
+{
+    class Program
+    {
+        static double carpan = 2;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Karekökünü almak istediğin sayıyı gir: ");
+
+                double a = double.Parse(Console.ReadLine());
+                
+                double root = squareRoot(a);
+                Console.WriteLine(root.ToString("G17"));
+
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+        static double squareRoot(double x)
+        {
+            double result = 1;
+
+            double difference = x - x / 2;
+            double a = 2;
+            double xb2 = x / a;
+
+
+
+            int counter = 0;
+            while (counter < 10000000)
+            {
+
+                if (xb2 * xb2 > x)
+                {
+                    xb2 = xb2 - difference / 2;
+                    result = xb2;
+                }
+                else if (xb2 * xb2 < x)
+                {
+                    xb2 = xb2 + difference / 2;
+                    result = xb2;
+                }
+                else if (xb2 * xb2 == x || xb2 * xb2 < x + 0.1 || xb2 * xb2 > x - 0.1)
+                {
+                    result = xb2;
+                    break;
+                }
+                difference = difference / 2;
+                counter++;
+            }
+
+
+            return result;
+        }
+
+
+    }
+}
